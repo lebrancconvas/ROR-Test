5fcb150a3781063abddfbbad3e82e25649dad79b
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-10 20:03:31 +0700

    Add handleClick State.

--- tarwanntest/src/header.js
+++ tarwanntest/src/header.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Header extends Component{
+  constructor(props) {
+        super(props)
+
+        this.state = {
+            growl: "คำถาม : ตาหวานเป็นคนยังไง ?"
+        }
+    }
+
+  handleClick() {
+      this.setState({ growl:"คำตอบ : เป็นคนดีด 2018 ไง ><" })
+  }
+
   render(){
     return(
         <div className="Header">
-          <h1>Tarwann BNK48 Official Fansite</h1>
+          <h1>Tarwann BNK48 Unofficial Fansite</h1>
+          <button onClick={this.handleClick.bind(this)}>เฉลย</button>
+          <h1>{this.state.growl}</h1>
           <h2>อิสราภา</h2>
         </div>
     );
