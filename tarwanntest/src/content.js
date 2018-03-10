25a3a9422d45fe63e425ab3263c2bde0d869b763
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-10 20:02:57 +0700

    Edit P Tag.

--- tarwanntest/src/content.js
+++ tarwanntest/src/content.js
@@ -6,7 +6,7 @@ class Content extends Component{
       <div className="Content">
         <h1>Hello Tarwann</h1>
         <button onClick={this.changetarwann}>Click สิ</button>
-        <p>The Official Website of Tarwann BNK48 Fanclub</p>
+        <p>The Official Website of Tarwann BNK48 Fanclub Naja</p>
         <p>{this.props.name}</p>
         <p>{this.props.price}</p>
         <a href="#">Hello</a>
