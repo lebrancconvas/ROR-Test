7e378b8126900dec3744f77c476de6148204a26d
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-10 20:02:23 +0700

    Move Component on App.js and Create new files.

--- tarwanntest/src/App.js
+++ tarwanntest/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Tarwannlogo from './assets/tarwann02.gif';
+import Header from './header.js';
+import Content from './content.js';
+import Footer from './footer.js';
 import './App.css';
 
 class App extends Component {
@@ -15,55 +18,15 @@ class App extends Component {
   }
 }
 
-class Header extends Component{
-  constructor(){
-    super();
-    this.state = {
-        name : "Tarwann Naja"
-    }
-  }
-  render(){
-    setInterval(()=>{
-        this.setState({name : "คนดีด 2018"});
-    },1000);
-    return(
-        <div className="Header">
-          <h1>Tarwann BNK48 Official Fansite</h1>
-          <h2>{this.state.name}</h2>
-        </div>
-    );
-  }
-}
+
+
+
 
 class Headimage extends Component{
   render(){
     return(
       <div>
         <img src={Tarwannlogo} className="App-logo" alt="logo" />
-      </div>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <div className="Content">
-        <h1>Hello Tarwann</h1>
-        <p>The Official Website of Tarwann BNK48 Fanclub</p>
-        <p>{this.props.name}</p>
-        <p>{this.props.price}</p>
-        <a href="#">Hello</a>
-      </div>
-    );
-  }
-}
-
-class Footer extends Component{
-  render(){
-    return(
-      <div className="Footer">
-        <h3>Copyright - BNK48</h3>
       </div>
     );
   }
