b94189abdeb326c91fca659b91c15d274a4bfba1
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-10 16:31:59 +0700

    Create Page1.

---
+++ tarwanntest/src/content1.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    alert("Page2 Test Success!!")
+    return(
+
+      <div className="container">
+        <img src={Tarwannlogo} className="App-logo" alt="logo" />
+        <h1>Hello Tarwann 2</h1>
+      </div>
+    );
+  }
+}
+
+export default Page2;
