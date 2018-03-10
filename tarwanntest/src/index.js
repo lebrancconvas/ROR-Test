2503bbefb3842f9e11f1661f520c69a88037a575
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-10 20:03:46 +0700

    Import to Route.

--- tarwanntest/src/index.js
+++ tarwanntest/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 import registerServiceWorker from './registerServiceWorker';
+import {Router,Route,Link,browserHistory} from 'react-router';
 
-ReactDOM.render( < App / > , document.getElementById('tarwannfansite'));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component="{App}" />
+  </Router>,document.getElementById('tarwannfansite')
+);
 registerServiceWorker();
